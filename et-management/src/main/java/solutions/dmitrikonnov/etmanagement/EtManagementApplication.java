7a978757909a939c2f2e6d3583fe1959cd08f677
Dmitri Konnov <lildkonnov.nnov@inbox.ru>
2022-10-07 19:04:01 +0400

    totally refactored

--- et-management/src/main/java/solutions/dmitrikonnov/etmanagement/EtManagementApplication.java
+++ et-management/src/main/java/solutions/dmitrikonnov/etmanagement/EtManagementApplication.java
@@ -1,9 +1,21 @@
 package solutions.dmitrikonnov.etmanagement;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EntityScan(basePackages = {
+        "solutions.dmitrikonnov.etentities",
+})
+@EnableAsync
+@EnableAspectJAutoProxy
+@EnableScheduling
+@OpenAPIDefinition
 public class EtManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(EtManagementApplication.class, args);
